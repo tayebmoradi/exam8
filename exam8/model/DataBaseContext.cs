a7c0471ea73b7d4859e50869955b453276b64708
tayeb <tayeb.mora69@gmail.com>
2023-08-15 19:00:50 +0330

    update

--- exam8/model/DataBaseContext.cs
+++ exam8/model/DataBaseContext.cs
@@ -1,6 +1,24 @@
-﻿namespace exam8.model
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace exam8.model
 {
-    public class DataBaseContext : dbcontext
+    public class DataBaseContext : DbContext
     {
+        public DataBaseContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        public DbSet<Period> Periods { get; set; }
+        public DbSet<Coustomer> Cookies { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
     }
 }
